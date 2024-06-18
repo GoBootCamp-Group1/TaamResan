1a2c4adef1f077988a2552d81885457aaeeff9a0
Saman <samankarbasi@live.com>
2024-06-18 19:20:59 +0330

    add phone validate method sample code to extract params from request body

--- api/tcp/handlers/handlers.go
+++ api/tcp/handlers/handlers.go
@@ -1,10 +1,10 @@
 package handlers
 
 import (
-	"net"
-
 	tcp "TaamResan/pkg/tcp_http_server"
 	"TaamResan/pkg/validator"
+	"encoding/json"
+	"net"
 )
 
 // HomeHandler handles requests to the home route.
@@ -16,21 +16,39 @@ func HomeHandler(conn net.Conn, request *tcp.Request) {
 	tcp.RespondJson(conn, responseData, 200)
 }
 
+type RequestBody struct {
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
 // TodoHandler handles requests to the //todo route.
 func TodoHandler(conn net.Conn, request *tcp.Request) {
 
-	email := "samankarbasi@live.com"
-
-	v := validator.Validate(email).
-		Email().
-		MinLength(5).
-		MaxLength(50).
-		NonEmpty()
-
-	if v.HasErrors() {
-		tcp.RespondJsonValidateError(conn, v.Errors(), 400)
+	var reqParams RequestBody
+	// Unmarshal the JSON body into the struct
+	err := json.Unmarshal([]byte(request.Body), &reqParams)
+	if err != nil {
+		tcp.RespondJsonError(conn, err.Error(), 500)
 		return
 	}
+
+	// Validate the email and phone
+	emailValidator := validator.Validate(reqParams.Email).Email()
+	phoneValidator := validator.Validate(reqParams.Phone).Phone()
+
+	// Collect all errors
+	errors := append(emailValidator.Errors(), phoneValidator.Errors()...)
+
+	// Respond with validation errors if any
+	if len(errors) > 0 {
+		tcp.RespondJsonValidateError(conn, errors, 422)
+		return
+	}
+
+	//if emailValidator.HasErrors() || phoneValidator.HasErrors() {
+	//	tcp.RespondJsonValidateError(conn, v.Errors(), 400)
+	//	return
+	//}
 
 	responseData := map[string]any{
 		"message": "Hello, TODO!",
